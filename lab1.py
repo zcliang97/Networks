829c98b6f0767f05dc9cf47b3f4d3508bb67ed50
ALEX CHIU <ackchiu@edu.uwaterloo.ca>
2019-01-29 20:36:24 -0500

    Adding Question 1

--- lab1.py
+++ lab1.py
@@ -1,7 +1,9 @@
 import numpy as np
+import time
+
 from DiscreteEventSimulator import DiscreteEventSimulator
 from DiscreteEventBufferSimulator import DiscreteEventBufferSimulator
-import time
+from ExponentialRandomVariableGenerator import ExponentialRandomVariableGenerator
 
 def generateGraph(lines, p_loss):
     # for (x, y) in lines:
@@ -9,6 +11,28 @@ def generateGraph(lines, p_loss):
     # plt.show()
     print lines
     print p_loss
+
+def question_1():
+    generator_lambda = 75
+    generator = ExponentialRandomVariableGenerator(lmbda=generator_lambda)
+
+    exponentialRandomVariables = []
+    # Generate 1000 Exponential Random Variables
+    for i in range(0, 999):
+        exponentialRandomVariables.append(generator.genValue())
+
+    # Compute Theoretical and Experimental Expected Value
+    theoretical_expected_value =  1.0 / generator_lambda
+    experimental_expected_value = np.mean(exponentialRandomVariables)
+    
+    # Compute Theoretical and Experimental Variance
+    theoretical_variance = 1.0/(generator_lambda * generator_lambda)
+    experimental_variance = np.var(exponentialRandomVariables)
+
+    print("Theoretical Expected Value: %f" % theoretical_expected_value)
+    print("Experimental Expected Value: %f" % experimental_expected_value)
+    print("Theoretical Variance: %f" % theoretical_variance)
+    print("Experimental Variance: %f" % experimental_variance)
 
 def question_3():
     rho_values = np.arange(0.25, 1.05, 0.1)
@@ -43,12 +67,14 @@ def question_6():
     generateGraph(lines, packet_loss)
 
 # main
-question_number = raw_input("Enter Question Number [3, 4, 6] ")
+question_number = raw_input("Enter Question Number [1, 3, 4, 6] ")
 question_number = int(question_number)
 
 start_time = time.time()
 
-if question_number == 3:
+if question_number == 1:
+    question_1()
+elif question_number == 3:
     question_3()
 elif question_number == 4:
     question_4()
