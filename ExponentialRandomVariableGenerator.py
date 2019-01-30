a6a931862d96b4d79d5986db64d90a1080411c64
ALEX CHIU <ackchiu@edu.uwaterloo.ca>
2019-01-30 11:55:47 -0500

    Cleanup ExponentialRandomVariableGenerator.py

--- ExponentialRandomVariableGenerator.py
+++ ExponentialRandomVariableGenerator.py
@@ -8,8 +8,8 @@ class ExponentialRandomVariableGenerator:
 
     def genValue(self):
         if (self.lmbda > 0):
+            # Generate uniform random variable and then use inverse method to calcualte exponential
             uniform_random_variable = np.random.uniform(0, 1)
             return (-1.0/self.lmbda) * math.log(1.0-uniform_random_variable)
         else:
             raise ValueError('Cannot generate exponential distribution with invalid lambda')
-            
