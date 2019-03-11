7eac0a80a29c9c1017a5b2a4c7e3605c60f76a3a
Frank Liang <fliang@Franks-MacBook-Pro.local>
2019-03-10 20:43:15 -0400

    updated word

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -6,14 +6,14 @@ from NonpersistentCSMASimulator import NonpersistentCSMASimulator
 from ExponentialRandomVariableGenerator import ExponentialRandomVariableGenerator
 
 def question_1():
-    #for A in [7, 10, 20]:
-    for N in [80, 100]:
-        simulator = PersistentCSMASimulator(N, 20).run()
+    for A in [7, 10, 20]:
+        for N in [80, 100]:
+            simulator = PersistentCSMASimulator(N, A).run()
 
 def question_2():
-    #for A in [7, 10, 20]:
-    for N in [20, 40, 60, 80, 100]:
-        simulator = NonpersistentCSMASimulator(N, 10).run()
+    for A in [7, 10, 20]:
+        for N in [20, 40, 60, 80, 100]:
+            simulator = NonpersistentCSMASimulator(N, A).run()
 
 # main
 question_number = raw_input("Enter Question Number [1, 2] ")
